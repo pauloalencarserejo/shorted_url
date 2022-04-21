bafa20a8d9ec880d2025f8ebdb23ceb64e2b921a
Paulo Alencar <paulovictoralencar.13@gmail.com>
2022-04-21 16:02:38 -0300

    refactor: removendo importação não utilizada

--- src/test/java/com/shorted/url/v1/controllers/ShortedControllerTest.java
+++ src/test/java/com/shorted/url/v1/controllers/ShortedControllerTest.java
@@ -19,12 +19,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Collections;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
