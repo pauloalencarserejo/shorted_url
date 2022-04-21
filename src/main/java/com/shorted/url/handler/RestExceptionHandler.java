d7d060945c3037dd1b7f207bb5843e763406da1e
Paulo Alencar <paulovictoralencar.13@gmail.com>
2022-04-21 13:51:48 -0300

    refactor: retirando importação não utilizada

--- src/main/java/com/shorted/url/handler/RestExceptionHandler.java
+++ src/main/java/com/shorted/url/handler/RestExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
