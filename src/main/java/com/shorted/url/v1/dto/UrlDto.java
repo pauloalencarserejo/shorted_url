f5a538e37d2fe320b2616c5239dddca3e1eae142
Paulo Alencar <paulovictoralencar.13@gmail.com>
2022-04-21 15:05:55 -0300

    refactor: adicionando construtores

--- src/main/java/com/shorted/url/v1/dto/UrlDto.java
+++ src/main/java/com/shorted/url/v1/dto/UrlDto.java
@@ -1,8 +1,12 @@
 package com.shorted.url.v1.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UrlDto {
     
     private String url;
